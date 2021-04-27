63fbbbaec5b72b0b7510f85bb806d1d463b19e54
Quang Du <phamquangdu1710@gmail.com>
2021-04-27 11:23:14 +0700

    Commit Add 6 new views

---
+++ src/views/AddBook.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="container">
+    <form class="add-book-form" action="" method="">
+      <div class="row back">
+        <a href=""><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+      </div>
+      <div class="row info">
+        <div class="col-lg-5 col-md-5 col-sm-5 col-5 ava">
+          <div class="row avatar">
+            <a href=""><i class="fa fa-plus" aria-hidden="true"></i></a>
+          </div>
+          <div class="row pd">
+            <div class="input-box">
+              <span class="d-block">Description:</span>
+              <textarea type="text" cols="100" rows="8"/>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-7 col-md-7 col-sm-7 col-7 input">
+          <div class="input-box">
+            <span>Title:</span>
+            <input type="text" placeholder="" v-model="title" />
+          </div>
+          <div class="input-box">
+            <span>Language:</span>
+            <input type="text" placeholder="" />
+          </div>
+          <div class="input-box">
+            <span>Author:</span>
+            <input type="text" placeholder="" />
+          </div>
+          <div class="input-box">
+            <span>Category:</span>
+            <input type="text" placeholder="" />
+          </div>
+          <div class="input-box">
+            <span>Department:</span>
+            <input type="text" placeholder="" />
+          </div>
+          <div class="input-box">
+            <span>Publication Date:</span>
+            <input type="text" placeholder="" />
+          </div>
+          <div class="input-box">
+            <span>Price:</span>
+            <input type="text" placeholder="" />
+          </div>
+          <div class="input-box">
+            <span>Rent Cost:</span>
+            <input type="text" placeholder="" />
+          </div>
+        </div>
+      </div>
+      <div class="row bt">
+        <button class="btn">
+          <a href="">Add Book <i class="fa fa-plus"></i></a>
+        </button>
+      </div>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddBook",
+};
+</script>
+
+<style lang="scss" scoped>
+.back {
+  margin-left: 1px;
+  margin-bottom: 5%;
+}
+
+i {
+  color: rgba(0, 0, 0, 0.38);
+}
+i:hover {
+  color: rgba(0, 0, 0, 0.87);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+}
+.container {
+  height: 76.3vh;
+  margin: 3% 0;
+}
+
+.add-book-form {
+  max-width: 550px;
+  width: 100%;
+  height: auto;
+  text-align: center;
+  background-color: #ffff;
+  border: 1px solid #e6e6e6;
+  border-radius: 25px;
+  padding: 30px 30px 20px;
+  box-shadow: 0px 10px 30px 10px rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+}
+
+.ava {
+  padding-left: 2%;
+  .avatar {
+    margin-left: 18%;
+    a {
+      width: 80px;
+      height: 100px;
+      background-color: rgba(0, 0, 0, 0.1);
+      i {
+        font-size: x-large;
+        margin-top: 50%;
+      }
+    }
+  }
+}
+.pd {
+  margin-top: 10%;
+
+  textarea {
+    margin-top: 10%;
+    border: rgba(0,0,0,0.25) solid 0.25px;
+    width: 150px;
+    height: auto;
+  }
+}
+
+.input .input-box {
+  margin-bottom: 8%;
+  text-align: left;
+}
+.input-box span {
+  color: rgba(0, 0, 0, 0.54);
+  display: inline;
+  font-size: small;
+  font-weight: bold;
+}
+.input-box input {
+  // margin-right: 10px;
+  width: 55%;
+  float: right;
+  font-size: small;
+  background: #fafafa;
+  border: 1px solid #cecece;
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: 1% 0% 1% 2%;
+}
+
+.bt {
+  margin-left: 170px;
+  margin-top: 2%;
+  margin-bottom: 5%;
+}
+button {
+  // text-align: center;
+  width: 40%;
+  background: #ecd4b4;
+  border: 1px solid #b7b7b7;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 12px;
+  padding: 1% 0%;
+  a {
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.38);
+    font-size: small;
+    // box-shadow: rgba(0, 0, 0, 0.38) ;
+  }
+
+  i {
+    color: rgba(0, 0, 0, 0.38);
+    font-size: small;
+  }
+}
+
+button:hover {
+  background: #ffe5c3;
+  color: rgba(0, 0, 0, 0.87);
+}
+</style>
