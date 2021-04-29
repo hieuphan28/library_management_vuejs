b04b74d1e61c942820b0e1ffc402ef9aaa0803c6
LAPTOP-R9O52LPL\Admin <deanlong1305@gmail.com>
2021-04-29 17:57:51 +0700

    commit revert aaaaa

--- src/views/BookInfo.vue
+++ src/views/BookInfo.vue
@@ -12,8 +12,10 @@ import BookProfies from "../components/BookProfies.vue";
 import Categories from "../components/Categories.vue";
 
 export default {
+  created () {
+  },
   components: { BookProfies, Categories },
-  name: "BookInfo",aaaaa
+  name: "BookInfo",
   
 };
 </script>
