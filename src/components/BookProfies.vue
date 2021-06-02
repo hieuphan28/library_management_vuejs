ddcf1ebcd6906daa0f3ef27d532896456405e677
LAPTOP-R9O52LPL\Admin <deanlong1305@gmail.com>
2021-06-02 16:40:48 +0700

    Fix Responsive Book Info

--- src/components/BookProfies.vue
+++ src/components/BookProfies.vue
@@ -4,12 +4,14 @@
       <div class="row">
         <div class="col-lg-5 col-md-12 col-sm-12 col-12 left-side">
           <div>
-            <div class="contain"><img class="book-cover" :src="bookInfo.thumbnail" alt="" /></div>
+            <div class="contain">
+              <img class="book-cover" :src="bookInfo.thumbnail" alt="" />
+            </div>
             <div class="row book-info">
-              <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+              <div class="col-lg-6 col-md-6 col-sm-3 col-3">
                 ${{ bookInfo.rent_cost }}
               </div>
-              <div class="col-lg-6 col-md-6 col-sm-6 col-6 book-left">
+              <div class="col-lg-6 col-md-6 col-sm-9 col-9 book-left">
                 Only ({{ bookLeft }}) books left
               </div>
             </div>
@@ -17,7 +19,7 @@
         </div>
         <div class="col-lg-7 col-md-12 col-sm-12 col-12 right-side">
           <h1>{{ bookInfo.book_name }}</h1>
-          <div class="des" >{{ bookInfo.description }}</div>
+          <div class="des">{{ bookInfo.description }}</div>
           <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-6 col-6 tip">
               <ul>
@@ -33,8 +35,17 @@
               <ul>
                 <li>{{ bookInfo.language }}</li>
                 <li>{{ bookInfo.author }}</li>
-                <li>{{ bookInfo.category?.category_name || bookInfo.category_name}}</li>
-                <li>{{ bookInfo.department?.department_name || bookInfo.department_name}}</li>
+                <li>
+                  {{
+                    bookInfo.category?.category_name || bookInfo.category_name
+                  }}
+                </li>
+                <li>
+                  {{
+                    bookInfo.department?.department_name ||
+                      bookInfo.department_name
+                  }}
+                </li>
                 <li>{{ bookQuantity }}</li>
                 <li>{{ bookInfo.publication_date }}</li>
               </ul>
@@ -50,17 +61,17 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { toastError, toastSuccess } from '../utilities/toast-util';
+import { mapGetters } from "vuex";
+import { toastError, toastSuccess } from "../utilities/toast-util";
 export default {
   name: "BookProfies",
-  props: ['book_id'],
+  props: ["book_id"],
   computed: {
     ...mapGetters("user", ["currentUser", "isMember"]),
     ...mapGetters({
-      getBookById: 'book/bookById',
-      bookItemQuantityByBookId: 'bookitem/bookItemQuantityByBookId',
-      bookItemAvailableByBookId: 'bookitem/bookItemAvailableByBookId',
+      getBookById: "book/bookById",
+      bookItemQuantityByBookId: "bookitem/bookItemQuantityByBookId",
+      bookItemAvailableByBookId: "bookitem/bookItemAvailableByBookId",
     }),
     bookInfo() {
       return this.getBookById(this.book_id) || {};
@@ -70,39 +81,43 @@ export default {
     },
     bookLeft() {
       return this.bookItemAvailableByBookId(this.book_id);
-    }
+    },
   },
   async mounted() {
     try {
-      await this.$store.dispatch('bookitem/getBookItemByBookId', this.book_id);
-      await this.$store.dispatch('book/getBookById', this.book_id);
-    } catch(e) {
+      await this.$store.dispatch("bookitem/getBookItemByBookId", this.book_id);
+      await this.$store.dispatch("book/getBookById", this.book_id);
+    } catch (e) {
       toastError(e);
     }
   },
   methods: {
     async AddToCart() {
       try {
-        await this.$store.dispatch('reservation/addCurrentCartItem', {
+        await this.$store.dispatch("reservation/addCurrentCartItem", {
           book_id: this.book_id,
         });
 
-        this.$store.dispatch('bookitem/getBookItemByBookId', this.book_id);
-        toastSuccess('Add item to cart successfully.');
-      } catch(e) {
+        this.$store.dispatch("bookitem/getBookItemByBookId", this.book_id);
+        toastSuccess("Add item to cart successfully.");
+      } catch (e) {
         toastError(e);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../scss/_variable.scss/";
 * {
   width: 100%;
+  @include mobile{
+    width: 13rem;
+  }
 }
 
-.container{
+.container {
   margin-top: 5%;
 }
 .left-side {
@@ -117,10 +132,14 @@ export default {
   justify-content: center;
   align-items: center;
   overflow: hidden;
-   img {
+  @include mobile {
+    width: 100px;
+    height: 120px;
+  }
+  img {
     flex-shrink: 15;
-    min-width: 100%;
-    min-height: 100%;
+    width: 100%;
+    height: 100%;
     top: 0;
     bottom: 0;
     left: 0;
@@ -129,7 +148,7 @@ export default {
   }
 }
 
-.des{
+.des {
   display: block;
   width: 100%;
   height: 90px;
@@ -149,6 +168,9 @@ export default {
     text-align: right;
     font-style: italic;
     color: rgba(0, 0, 0, 0.5);
+    @include mobile{
+      width: 3rem;
+    }
   }
 }
 
@@ -159,7 +181,7 @@ button {
   width: 150px;
   text-decoration: none;
   color: rgba(0, 0, 0, 0.54);
-    transition: 0.3s;
+  transition: 0.3s;
   i {
     position: relative;
     width: auto;
@@ -172,12 +194,14 @@ button:hover {
   color: rgba(0, 0, 0, 0.87);
 }
 
-
 ul {
   padding: 0;
   li {
     list-style: none;
     padding: 5px 0 8px 0;
+    @include mobile {
+      width: 5rem;
+    }
   }
 }
 
@@ -186,9 +210,15 @@ ul {
     font-size: xx-large;
     font-weight: bold;
     margin-bottom: 1%;
+    @include mobile {
+      font-size:x-large;
+    }
   }
   .tip {
     font-weight: bold;
+    @include mobile {
+      padding-right: 1rem;
+    }
   }
 }
 </style>
